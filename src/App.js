67937e161c04f5a3a8471b62869fb1d997e4c3d2
Vardges <vkohovhannisyan96@gmail.com>
2018-11-19 22:09:41 +0400

    new dish add

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import AdminHomePage from './components/AdminHomePage/AdminHomePage';
 import EditAdmin from './components/EditAdmi/EditAdmin';
 import EditManu from './components/Editmanu/EditManu';
 import TableInfo from './components/AdminHomePage/Tablelist/TableInfo/TableInfo';
+import AddNewDish from './components/AddNewDish/AddNewDish';
+
 import './App.css';
 
 
@@ -23,6 +25,7 @@ class App extends Component {
                         <Route path='/admin/editadmin' component={EditAdmin} />
                         <Route path='/admin/editmanu' component={EditManu} />
                         <Route path='/admin/table/:tableId' component={TableInfo} />
+                        <Route path='/newDish' component={AddNewDish}/>
                     </Switch>
                 </div>
             </Router>
