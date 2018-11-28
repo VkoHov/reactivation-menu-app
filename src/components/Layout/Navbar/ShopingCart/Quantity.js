4b0fb7df1c913444b36e32d1c322b7bd87a9a9fb
Vardges <vkohovhannisyan96@gmail.com>
2018-11-28 20:53:04 +0400

    merging

--- src/components/Layout/Navbar/ShopingCart/Quantity.js
+++ src/components/Layout/Navbar/ShopingCart/Quantity.js
@@ -1,48 +1,37 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+
 class Quantity extends Component {
     state = {
         count: this.props.count,
         price: this.props.price,
-    }
-    minusCount = e => {
+    };
+    minusCount = () => {
         if (this.state.count > 1) {
-          this.setState({
-            count: this.state.count - 1
-          });
+            this.setState({
+                count: this.state.count - 1
+            });
         }
-      };
-    
-      plusCount = e => {
-         
+    };
+
+    plusCount = () => {
+
         this.setState({
-          count: this.state.count + 1
+            count: this.state.count + 1
         });
-      };
-    render() { 
-        if(this.state.count > 1){
-            return (<div>
-            
-                <span>SUBTOTAL: {this.state.price * this.state.count}(AMD)</span> 
+    };
+
+    render() {
+        return (
+            <div>
+                <span>SUBTOTAL: {this.state.price * this.state.count}(AMD)</span>
                 <button className="count-button" onClick={this.minusCount}> -
-                    </button>
-                    <button className="count-button">{this.state.count}</button>
-                    <button className="count-button" onClick={this.plusCount}> +
-                    </button>
-                 </div>
-             );
-        }else{
-            return(
-                <div>
-                <button className="count-button" onClick={this.minusCount}> -
-                    </button>
-                    <button className="count-button">{this.state.count}</button>
-                    <button className="count-button" onClick={this.plusCount}> +
-                    </button>
-                </div>
-            )
-        }
-        
+                </button>
+                <button className="count-button">{this.state.count}</button>
+                <button className="count-button" onClick={this.plusCount}> +
+                </button>
+            </div>
+        );
     }
 }
- 
+
 export default Quantity;
--- src/components/Layout/Navbar/ShopingCart/ShoppingCart.js
+++ src/components/Layout/Navbar/ShopingCart/ShoppingCart.js
@@ -1,58 +1,46 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
 import Quantity from './Quantity';
 
 
+class ShoppingCart extends Component {
 
- class ShoppingCart extends Component {
-     
-        
-        
-        
-     
-     
-     render() { 
+
+    render() {
         let dishInfo = JSON.parse(sessionStorage.getItem('dishInfo'));
-        
-console.log('shopping cart dishinfo',dishInfo)
-  
-        if(dishInfo){
-            return ( 
-                <div>My CART 
-                   {
-                       
-                       dishInfo.map((dish,index)=>{
-                           return(
-                               <div key = {index}>
-                               <div>{dish.title}</div>
-                               <div>Description: {dish.description}</div>
-                               <div>Unit Price: {dish.price}</div>
-                               <div>QUANTITY: {dish.count}</div>
-                               <Quantity count = {dish.count} price = {dish.price}/>
-                               <div>Rating: {dish.rating}</div>
-                              
-                               
-                               
-                              
-                             </div>
-                           )
-                       })
-                   }
-                  
-                </div> );
-        }else{
-            return(
+        if (dishInfo) {
+            return (
+                <div>My CART
+                    {
+                        dishInfo.map((dish, index) => {
+                            return (
+                                <div key={index}>
+                                    <div>{dish.title}</div>
+                                    <div>Description: {dish.description}</div>
+                                    <div>Unit Price: {dish.price}</div>
+                                    <div>QUANTITY: {dish.count}</div>
+                                    <Quantity count={dish.count} price={dish.price}/>
+                                    <div>Rating: {dish.rating}</div>
+                                </div>
+                            )
+                        })
+                    }
+
+                </div>
+            );
+        } else {
+            return (
                 <div>SHOPPING CART IS EMPTY</div>
             )
         }
-        
-     }
- }
-  const mapStateToProps = (state) => {
-      return{
-        dishInfo: state,
-      }
-      
-  }
 
- export default connect(mapStateToProps)(ShoppingCart);
+    }
+}
+
+const mapStateToProps = (state) => {
+    return {
+        dishInfo: state,
+    }
+}
+
+export default connect(mapStateToProps)(ShoppingCart);
