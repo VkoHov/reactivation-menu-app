52a0e206a206f4cd9fcaf89fcfcdfd73d6a833df
Vardges <vkohovhannisyan96@gmail.com>
2018-12-09 15:31:16 +0400

    bug

--- src/actions/addToFavAction.js
+++ src/actions/addToFavAction.js
@@ -1,33 +1,25 @@
-export const addFavToFireStore = (info)=>{
-    return (dispatch, getState, {getFirestore}) =>{
+export const addFavToFireStore = (info) => {
+    return (dispatch, getState, {getFirestore}) => {
         const firestore = getFirestore();
-        if(info.id){
-            firestore.collection("users").doc(info.id).update({
-                favorites: firestore.FieldValue.arrayUnion(info)})
-    
-                .then(() => {
-                    dispatch({type: 'ADD_TO_FAVORITES', info});
-                })
-        }else {
-            alert("Please login to add favorites")
-     
-        }
-        
-            // .catch(err => {
-            //     dispatch({ type: 'DELETE_INFO_ERROR' }, err);
-            // });
+        firestore.collection("users").doc(info.id).update({
+            favorites: firestore.FieldValue.arrayUnion(info)
+        })
+        .then(() => {
+            dispatch({type: 'ADD_TO_FAVORITES', info});
+        }).catch(err => {
+            dispatch({type: 'ADD_TO_FAVORITES_ERROR', err});
+        });
     }
-
-}
-export const removeFromFirestore = (info) =>{
-    return(dispatch,getState,{getFirestore}) =>{
+};
+export const removeFromFirestore = (info) => {
+    return (dispatch, getState, {getFirestore}) => {
         const firestore = getFirestore();
 
         firestore.collection("users").doc(info.id).update({
             favorites: info.filteredFavs
         }).then(() => {
-            dispatch({type: 'REMOVE_FROM_FAVORITES', info},console.log("removed"));
+            dispatch({type: 'REMOVE_FROM_FAVORITES', info}, console.log("removed"));
         })
 
     }
-}
+};
